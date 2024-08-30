955e3396ab4233fb91c7cf3282e77d6ae53752b2
lformank <ladalizer28@email.cz>
2024-08-30 15:53:52 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: lformank <lformank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 17:32:10 by lformank          #+#    #+#             */
-/*   Updated: 2024/08/27 15:13:38 by lformank         ###   ########.fr       */
+/*   Updated: 2024/08/30 15:53:17 by lformank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s)
+int	ft_strlen(const char *s)
 {
 	int	i;
 
@@ -22,12 +22,13 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
-void	*ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(int nmemb, int size)
 {
 	char	*ptr;
-	size_t	i;
+	int		i;
 
 	i = 0;
+	ptr = 0;
 	ptr = (char *)malloc(nmemb * size);
 	if (ptr == 0)
 		return (0);
@@ -57,45 +58,90 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	while (s2[i] != '\0')
 		dest[j++] = s2[i++];
 	dest[j] = '\0';
+	free ((void *)s1);
 	return (dest);
 }
 
-/*int	ft_search_newline(int newline, char *storage)
+void	putstr(char *s, int len)
 {
+	int	i;
 
-}*/
-
-char	*get_text_stored(int fd, char *storage, char *buffer)
-{
-	int		n;
-	int		printed;
-	int		zero;
-	int		newline;
-
-	n = 6;
-	buffer = 0;
-	printed = 0;
-	zero = n;
-	newline = 0;
-	while (zero == n)
+	i = 0;
+	while (i <= len || s[i] != '\0')
 	{
-		zero = read(fd, buffer, n);
-		printed += zero;
+		write(1, &s[i], 1);
+		i++;
+	}
+}
+
+int	search_newline(char *storage)
+{
+	int			i;
+
+	i = 0;
+	while (i <= BUFFER_SIZE || storage[i] != '\0')
+	{
+		if (storage[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+char	*save_rest(char *stack, int position)
+{
+	char	*dest;
+	int		len;
+	int		i;
+
+	len = ft_strlen(stack);
+//	printf("%d,", len);
+	position++;
+	dest = (char *)malloc((len - position) * sizeof(char));
+	i = 0;
+	while (stack[position] != '\0' && position < len)
+	{
+		dest[i] = stack[position];
+		position++;
+		i++;
+//		printf("%s.", dest);
+	}
+	free(stack);
+	printf("\n%s/", dest);
+	return (dest);
+}
+
+char	*get_text_stored(int fd, char *stack, char *buffer)
+{
+	int		i;
+	int		zero;
+	int		position;
+
+	i = 0;
+	zero = BUFFER_SIZE;
+	position = 1;
+	while (zero == BUFFER_SIZE && position != 0)
+	{
+		zero = read(fd, buffer, BUFFER_SIZE);
 		if (zero != 0)
-			storage = ft_strjoin(storage, buffer);
-//		newline = ft_search_newline(newline, storage);
+		{
+			stack = ft_strjoin(stack, buffer);
+			position = search_newline(stack);
+			putstr(stack, position);
+			stack = save_rest(stack, position);
+		}
 	}
 	free (buffer);
-	return (storage);
+	return (stack);
 }
 
 char	*get_next_line(int fd)
 {
-	char	*storage;
+	static char	*storage;
 	char	*buffer;
 
-	storage = malloc(1 * sizeof(char));
-	buffer = malloc(1 * sizeof(char));
+	storage = (char *)malloc(1 * sizeof(char));
+	buffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
 	if (!buffer || !storage)
 		return (0);
 	storage = get_text_stored(fd, storage, buffer);
@@ -105,9 +151,17 @@ char	*get_next_line(int fd)
 int	main(void)
 {
 	int	fd;
+	int	i;
 
+	i = 0;
+	fd = 0;
 	fd = open("private.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
+	while (i < 2)
+	{
+		printf("%s!", get_next_line(fd));
+		i++;
+	}
 	close(fd);
 	return (0);
 }
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,17 @@
 /*   By: lformank <lformank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 13:51:36 by lformank          #+#    #+#             */
-/*   Updated: 2024/08/27 14:48:38 by lformank         ###   ########.fr       */
+/*   Updated: 2024/08/28 12:10:54 by lformank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+
+#endif
 
 # include <unistd.h>
 # include <stdio.h>
